4b0213451802b58a21143dad6191595d37d71de4
Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>
2022-12-16 18:28:45 +0100

    Added linear test circuits to CMakeLists.txt; Added SSN models to Components.h
    Signed-off-by: Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>

--- dpsim-models/include/dpsim-models/Components.h
+++ dpsim-models/include/dpsim-models/Components.h
@@ -83,6 +83,7 @@
 #include <dpsim-models/EMT/EMT_Ph1_VoltageSource.h>
 #include <dpsim-models/EMT/EMT_Ph1_VoltageSourceRamp.h>
 #include <dpsim-models/EMT/EMT_Ph1_VoltageSourceNorton.h>
+#include <dpsim-models/EMT/EMT_Ph1_SSN_Full_Serial_RLC.h>
 
 #include <dpsim-models/EMT/EMT_Ph3_Capacitor.h>
 #include <dpsim-models/EMT/EMT_Ph3_Inductor.h>
@@ -101,6 +102,9 @@
 #include <dpsim-models/EMT/EMT_Ph3_SynchronGenerator6bOrderVBR.h>
 #include <dpsim-models/EMT/EMT_Ph3_SynchronGeneratorDQ.h>
 #include <dpsim-models/EMT/EMT_Ph3_SynchronGeneratorDQTrapez.h>
+#include <dpsim-models/EMT/EMT_Ph3_SSN_Capacitor.h>
+#include <dpsim-models/EMT/EMT_Ph3_SSN_Inductor.h>
+#include <dpsim-models/EMT/EMT_Ph3_SSN_Full_Serial_RLC.h>
 #ifdef WITH_SUNDIALS
   #include <dpsim-models/EMT/EMT_Ph3_SynchronGeneratorDQODE.h>
 #endif
