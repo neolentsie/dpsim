6c77bbd3fd4a916fe34fd9decc6a480e2004aadc
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-03-07 11:11:09 +0100

    Use precompilation-statements when building library
    Signed-off-by: Felix Wege <fwege@eonerc.rwth-aachen.de>

--- dpsim/src/SolverPlugins/julia/JuliaMNA/build/build.jl
+++ dpsim/src/SolverPlugins/julia/JuliaMNA/build/build.jl
@@ -9,6 +9,7 @@ const target_dir = ARGS[1]
 println("Creating Julia MNA solver library in $target_dir")
 PackageCompiler.create_library("$(build_dir)/..", target_dir;
                                 lib_name="juliamna",
+                                precompile_execution_file="$(@__DIR__)/precompile_statements.jl",
                                 incremental=true,
                                 filter_stdlibs=true,
                                 header_files = ["$(@__DIR__)/juliamna.h"],
