bf76d219a3b7cd9455cf5d4bc2af9b2f37d798b4
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-03-29 16:02:14 +0200

    Disable GC for test interface
    Disabling the GC before passing the system matrix pointer to init()
        ensures that the correct value is read from the plugin
    
    Signed-off-by: Felix Wege <fwege@eonerc.rwth-aachen.de>

--- dpsim/src/SolverPlugins/julia/JuliaMNA/test/test_inteface.jl
+++ dpsim/src/SolverPlugins/julia/JuliaMNA/test/test_inteface.jl
@@ -1,5 +1,5 @@
 using JuliaMNA
-
+using Profile
 
 struct ArrayPath path::String end
 struct VectorPath path::String end
@@ -36,42 +36,28 @@ function read_input(path::VectorPath)
     rhs_vector = parse.(Float64, split(rhs_vector_strings[1]))
 end
 
-# # Read system matrix from file
-# # system_matrix_strings = readlines("test/system_matrix_small.txt")
-# system_matrix_strings = readlines("../system_matrix.txt")
-
-# # Sanize strings
-# system_matrix_strings = replace.(system_matrix_strings, r"[\[\],]" => "")
-
-# # Convert system to dpsim_csr_matrix
-# values = parse.(Float64, split(system_matrix_strings[1]))
-# rowIndex = parse.(Cint, split(system_matrix_strings[2]))
-# colIndex = parse.(Cint, split(system_matrix_strings[3]))
-
-# system_matrix = dpsim_csr_matrix(
-#     Base.unsafe_convert(Ptr{Cdouble}, values),
-#     Base.unsafe_convert(Ptr{Cint}, rowIndex),
-#     Base.unsafe_convert(Ptr{Cint}, colIndex),
-#     parse(Int32, system_matrix_strings[4]),
-#     parse(Int32, system_matrix_strings[5])
-# )
-
-# # Read right hand side vector from file
-# # rhs_vector_strings = readlines("test/rhs_small.txt")
-# rhs_vector_strings = readlines("../rhs.txt")
-
-# # Sanize rhs strings and parse into Float64 vector
-# rhs_vector_strings = replace.(rhs_vector_strings, r"[\[\],]" => "")
-# rhs_vector = parse.(Float64, split(rhs_vector_strings[1]))
-
-# Inialize left hand side vector
-
-system_matrix = read_input(ArrayPath("test/system_matrix.txt"))
+GC.enable(false) # We cannot be sure that system_matrix is garbage collected before the pointer is passed... 
+system_matrix = read_input(ArrayPath("test/system_matrix_small.txt"))
 system_matrix_ptr = pointer_from_objref(system_matrix)
 
-rhs_vector = read_input(VectorPath("test/rhs.txt"))
+@show unsafe_wrap(Array, system_matrix.values, system_matrix.nnz)
+
+rhs_vector = read_input(VectorPath("test/rhs_small.txt"))
 
 lhs_vector = zeros(Float64, length(rhs_vector))
 
 init(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, system_matrix_ptr))
+GC.enable(true)
+
 @time solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vector), Base.unsafe_convert(Ptr{Cdouble}, lhs_vector))
+# @profile solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vector), Base.unsafe_convert(Ptr{Cdouble}, lhs_vector))
+
+
+# ## Profling
+# @time begin
+#     @profile begin
+#         for i in 1:1000
+#             solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vector), Base.unsafe_convert(Ptr{Cfloat}, lhs_vector))
+#         end
+#     end
+# end
